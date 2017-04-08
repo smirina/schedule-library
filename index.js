908e7c881e5b9ec7b07521718f19de77be4aa0f9
Smirina <nitchenkoiv@gmail.com>
2017-04-08 13:46:31 +0300

    Add empty methods

---
+++ index.js
@@ -0,0 +1,35 @@
+function showByDate(date) {
+
+}
+
+function showByDateInterval(date1, date2) {
+
+}
+
+function showByDateInterval(date1, date2) {
+
+}
+
+function addEvent() {
+
+}
+
+function changeEvent() {
+
+}
+
+function addSchool() {
+
+}
+
+function changeSchool() {
+
+}
+
+function addPlace() {
+
+}
+
+function changePlace() {
+
+}
