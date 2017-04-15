c02dad562dcab278f80264fc1e679a77e1b79107
Smirina <nitchenkoiv@gmail.com>
2017-04-15 17:33:48 +0300

    Add data

--- index.js
+++ index.js
@@ -1,45 +1,109 @@
 function schedule () {
 
-  function showByDate(date) {
+  var nextID = 1;
+
+  function getNextID () {
+    return nextID++
+  }
+
+  function getByDate(date) {
+    return eventsByDate[date].map(function(id){
+      let ev = Object.assign({},events[id],{
+        speaker: speakers[events[id].speaker],
+        place: places[events[id].place],
+        school: schools[events[id].school]
+      })
+      return ev
+    })
+  }
+
+  function getByDateInterval(date1, date2) {
 
   }
 
-  function showByDateInterval(date1, date2) {
+  function addEvent(data) {
+    var id = data.id || getNextID()
+    data.id = id
+    events[id] = data
 
-  }
-
-  function addEvent() {
-
+    var day = data.date.split('T')[0]
+    if (!eventsByDate[day]) {eventsByDate[day] = []}
+    eventsByDate[day].push(id)
+    return id
   }
 
   function changeEvent() {
 
   }
 
-  function addSchool() {
-
+  function addSchool(data) {
+    var id = data.id || getNextID()
+    data.id = id
+    schools[id] = data
+    return id
   }
 
   function changeSchool() {
 
   }
 
-  function addPlace() {
-
+  function addPlace(data) {
+    var id = data.id || getNextID()
+    data.id = id
+    places[id] = data
+    return id
   }
 
   function changePlace() {
 
   }
 
+  function addSpeaker(data) {
+    var id = data.id || getNextID()
+    data.id = id
+    speakers[id] = data
+    return id
+  }
+
+  function changeSpeaker() {
+
+  }
+
+  function serialize() {
+    return JSON.stringify({
+      events,
+      schools,
+      speakers,
+      places
+    })
+  }
+  function deserialize(data) {
+    var parsed = JSON.parse(data)
+    Object.assign(speakers, parsed.speakers)
+    Object.assign(places, parsed.places)
+    Object.assign(schools, parsed.schools)
+    Object.assign(events, parsed.events)
+  }
+
+
+  var speakers = {}
+  var schools = {}
+  var places = {}
+  var events = {}
+  var eventsByDate = {}
+
   return {
-    showByDate: showByDate,
-    showByDateInterval: showByDateInterval,
+    getByDate: getByDate,
+    getByDateInterval: getByDateInterval,
     addEvent: addEvent,
     changeEvent: changeEvent,
     addSchool: addSchool,
     changeSchool: changeSchool,
     addPlace: addPlace,
-    changePlace: changePlace
+    changePlace: changePlace,
+    addSpeaker: addSpeaker,
+    changeSpeaker: changeSpeaker,
+    serialize: serialize,
+    deserialize: deserialize
   }
 }
---
+++ test.js
@@ -0,0 +1,90 @@
+var myData = JSON.stringify({
+  events: {
+    1: {
+      name: 'Еще лекция',
+      school: '1',
+      place: '1',
+      speaker: '1',
+      date: '2017-05-02T19:00:00'
+    },
+    2: {
+      name: 'Про дизайн',
+      school: '2',
+      place: '1',
+      speaker: '3',
+      date: '2017-05-03T19:00:00'
+    },
+    3: {
+      name: 'Про дизайнеров',
+      school: 'sh-ti',
+      place: '2',
+      speaker: '2',
+      date: '2017-05-03T19:00:00'
+    }
+  },
+  schools: {
+    'sh-ti': {
+      id: 'sh-ti',
+      name: 'Школа тестирования',
+      students: 32
+    },
+    'sh-dis': {
+      id: 'sh-dis',
+      name: 'Школа мобильного дизайна',
+      students: 28
+    },
+    'sh-ri': {
+      id: 'sh-ri',
+      name: 'Школа разработки интерфейсов',
+      students: 32
+    }
+  },
+  places: {
+    1: {
+      name: 'Зеленая альпака',
+      seats: 30
+    },
+    2: {
+      name: 'Желтоватый слон',
+      seats: 32
+    },
+    3: {
+      name: 'Синий кит',
+      seats: 40
+    }
+  },
+  speakers: {
+    grlv: {
+      name: 'Андрей Гурылев',
+      details: 'Wrike'
+    },
+    gktlh: {
+      name: 'Ктулху',
+      details: 'Неописуем'
+    },
+    zen: {
+      name: 'Алиса Зен',
+      details: 'Самый главный лис'
+    },
+  }
+})
+
+var mySchedule = schedule()
+
+var mySchool = mySchedule.addSchool({name: 'Школа тестирования', students: 32})
+
+var myPlace = mySchedule.addPlace({name: 'Зеленая альпака', seats: 30})
+
+var mySpeaker = mySchedule.addSpeaker({name: 'Ктулху', details: 'Неописуем'})
+
+var myEvent = mySchedule.addEvent({
+  name: 'Первая лекция',
+  school: mySchool,
+  place: myPlace,
+  speaker: mySpeaker,
+  date: '2017-05-01T19:00:00'
+})
+
+var result = mySchedule.getByDate('2017-05-01')
+
+console.log('hello: ',result)
